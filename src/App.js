47934d199c92fb0eef5d784e3f2cd41eaa0fae78
Junior Oliveira <arojunior@gmail.com>
2017-01-12 15:25:59 -0200

    route link from index to contact

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router'
 import logo from './logo.svg';
 import './App.css';
 
@@ -12,6 +13,9 @@ class App extends Component {
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+            <Link to="/contact">Go to Contact Page</Link>
         </p>
       </div>
     );
