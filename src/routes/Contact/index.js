9ac4d1a4f95577782a0786ddb19e58af6aac2978
Junior Oliveira <arojunior@gmail.com>
2017-01-12 15:18:37 -0200

    contact form container created

---
+++ src/routes/Contact/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
+
+class Contact extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row text-center">
+                    <h2>Contact</h2>
+                </div>
+                <div></div>
+            </div>
+        )
+    }
+}
+
+export default Contact
