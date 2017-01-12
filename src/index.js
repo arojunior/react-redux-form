b673eedbf16ef453627a6b3010cca6caad1e8155
Junior Oliveira <arojunior@gmail.com>
2017-01-12 15:24:45 -0200

    app integrations between react and redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,46 @@
+/*
+* React
+*/
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import { render } from 'react-dom'
+import { Router, Route, browserHistory } from 'react-router'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+/*
+* Redux
+*/
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
+import thunk from 'redux-thunk'
+import { reducer as formReducer } from 'redux-form'
+
+/*
+* Reducers
+*/
+const combineReducer = combineReducers({
+    form : formReducer
+})
+
+/*
+* Store
+*/
+const store = createStore(combineReducer, {}, compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+))
+
+/*
+* App
+*/
+import Contact from './routes/Contact'
+import App from './App'
+import './index.css'
+
+/*
+* Render App
+*/
+render(<Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path="/" component={ App } />
+      <Route path="/contact" component={ Contact } />
+    </Router>
+</Provider>, document.getElementById('root'))
