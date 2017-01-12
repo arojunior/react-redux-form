69dd061c58a94d445c2121a9422caa307d17c013
Junior Oliveira <arojunior@gmail.com>
2017-01-12 15:28:39 -0200

    contact form result to screen

--- src/routes/Contact/index.js
+++ src/routes/Contact/index.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react'
+import Form from './components/Form'
+import { reset } from 'redux-form'
 import 'bootstrap/dist/css/bootstrap.css'
 
 class Contact extends Component {
-    handleSubmit(values, dispatch) {
-        console.log(values)
+    
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            formData : null
+        }
+
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
+
+    handleSubmit(values, dispatch) {
+        this.setState({ formData : values })
+
+        dispatch(reset('contactForm'))
+    }
+
     render() {
+        const { formData } = this.state
+
         return (
             <div className="container">
                 <div className="row text-center">
                     <h2>Contact</h2>
                 </div>
                 <Form onSubmit={this.handleSubmit} />
-                <div></div>
+                <div>Form data: { JSON.stringify(formData) }</div>
             </div>
         )
     }
